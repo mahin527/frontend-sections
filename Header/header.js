9589384d572f819241e91336c99abf05bc4486ba
mahin527 <hasan.mahin527@gmail.com>
2025-07-18 14:21:47 +0600

    created js file

---
+++ Header/header.js
@@ -0,0 +1,19 @@
+const navmenuBtn = document.getElementById('navmenuBtn');
+const navmenuBtnIcon = document.querySelector('#navmenuBtn i');
+console.log(navmenuBtnIcon);
+
+const navmenu = document.getElementById('navmenu');
+const showNavmenu = () => {
+  if (navmenu.classList.contains('left-[-100%]')) {
+    navmenu.classList.remove('left-[-100%]');
+    navmenu.classList.add('left-[20%]');
+    navmenuBtnIcon.classList.remove('ri-menu-2-line');
+    navmenuBtnIcon.classList.add('ri-close-fill');
+  } else {
+    navmenu.classList.remove('left-[20%]');
+    navmenu.classList.add('left-[-100%]');
+    navmenuBtnIcon.classList.remove('ri-close-fill');
+    navmenuBtnIcon.classList.add('ri-menu-2-line');
+  }
+};
+navmenuBtn.addEventListener('click', showNavmenu);
